8c2a6b6fcde6c469c2d7d9ddc2fa3d1956486501
Shwetha satish <shwethasatish1@gmail.com>
2022-11-15 21:57:00 +0530

    commit2

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+import { Component } from 'react/cjs/react.production.min';
+//import { createStackNavigator, createAppContainer } from 'react-navigation'; 
+import SecondPage from './SecondPage'
+const Separator = () => (
+  <View style={styles.separator} />
+);
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>School contests</Text>
-    </View>
-  );
+class LoginScreen extends Component {  
+  render() {  
+    const App = () => (
+    <SafeAreaView style={styles.container}>
+     <View style={styles.container}>
+        <Text>Schoolcontests</Text>
+      </View>
+     <View>
+        <Button
+          title="Sign in using Facebook"
+          onPress={open={SecondPage}}></Button>
+          <Text style={styles.buttonText}>Next Page</Text>
+      </View>
+      <Separator />
+      <View>
+        <Button
+          title="Sign in using google"
+          onPress={() => Alert.alert('Simple button pressed')}
+        />
+      </View>
+    </SafeAreaView>
+    );
+  }
+}
+class SecondPage extends Component{
+  render(){
+    <Text>New</Text>
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +44,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonContainer: {
+    marginTop: 10,
+  },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
 });
+
+export default App;
